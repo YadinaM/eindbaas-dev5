285dd9d242c61892a1ad62bae8950e229e611411
YadinaM <yadina.moreira@gmail.com>
2023-11-27 16:08:08 +0100

    feat: add structure for users in API

---
+++ models/User.js
@@ -0,0 +1,17 @@
+// User model idea (no mongoose yet)
+const User = {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  };
+
+module.exports = User;
---
+++ routes/api/v1/users.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+const usersController = require("../../../controllers/api/v1/users");
+
+module.exports = router;
