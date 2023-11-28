2f38ab444c66459d2490eaf57fc47404b91a9022
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-11-28 14:53:15 +0100

    feat: create mongoose schema for user

--- models/User.js
+++ models/User.js
@@ -1,17 +1,18 @@
-// User model idea (no mongoose yet)
-const User = {
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const UserSchema = new Schema ({
     username: {
       type: String,
       required: true,
     },
     admin: {
-      type: String,
+      type: Boolean,
       required: true,
     },
     password: {
       type: String,
       required: true,
     },
-  };
-
+});
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
