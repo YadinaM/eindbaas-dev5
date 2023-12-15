38405132a21448cbccb0f7f56e94456cfa10f2e2
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-12-15 17:14:29 +0100

    feat: add authorization to routes

--- routes/api/v1/shoes.js
+++ routes/api/v1/shoes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const shoesController = require("../../../controllers/api/v1/shoes");
+const authorizeAdmin = require('../../../middleware/authorization');
 
 //get shoes 
-router.get("/", shoesController.index);
-router.get("/:id", shoesController.indexID);
-router.put("/:id", shoesController.update);
+router.get("/", authorizeAdmin, shoesController.index);
+router.get("/:id", authorizeAdmin, shoesController.indexID);
+router.put("/:id", authorizeAdmin, shoesController.update);
 router.post("/", shoesController.create);
-router.delete("/:id", shoesController.remove);
-router.patch('/:id/status', shoesController.editStatus);
+router.delete("/:id", authorizeAdmin, shoesController.remove);
+router.patch('/:id/status', authorizeAdmin, shoesController.editStatus);
 
 module.exports = router;
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const usersController = require("../../../controllers/api/v1/users");
+const authorizeAdmin = require('../../../middleware/authorization');
 
-router.get("/", usersController.index);
-router.get("/:id", usersController.indexID);
+router.get("/", authorizeAdmin, usersController.index);
+router.get("/:id", authorizeAdmin, usersController.indexID);
 router.post("/", usersController.create);
-router.delete("/:id", usersController.remove);
-router.put("/:id", usersController.update);
+router.delete("/:id", authorizeAdmin, usersController.remove);
+router.put("/:id", authorizeAdmin, usersController.update);
 router.post('/login', usersController.login);
 
 module.exports = router;
