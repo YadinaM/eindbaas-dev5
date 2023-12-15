72e957cb77fb206a46a62b9c70a013ec6b9fe6fd
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-12-15 17:13:35 +0100

    feat: add authorization

---
+++ middleware/authorization.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+
+//require dotenv
+require("dotenv").config();
+const secretKey = process.env.JWT_SECRET;
+
+const authorizeAdmin = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({
+      status: "error",
+      message: "No token provided",
+    });
+  }
+
+  // No bearer in token
+  const extractToken = token.replace("Bearer ", "");
+
+  jwt.verify(extractToken, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        status: "error",
+        message: "Invalid token",
+      });
+    }
+
+    if (!decoded.admin) {
+      return res.status(403).json({
+        status: "error",
+        message: "Forbidden: User is not an admin",
+      });
+    }
+
+    req.user = decoded; // Attach user details to the request object
+
+    next();
+  });
+};
+
+module.exports = authorizeAdmin;
